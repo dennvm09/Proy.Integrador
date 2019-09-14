bfc09bd41b4e10ee90a70da7fa3d4f4cf45388c3
vardo159 <braianelsociales@gmail.com>
2019-09-14 14:02:15 -0500

    the model package is created and tested

--- Proy_In/Program.cs
+++ Proy_In/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using model;
 
 namespace Proy_In
 {
@@ -14,6 +15,10 @@ namespace Proy_In
         [STAThread]
         static void Main()
         {
+
+
+            Class1.methodTest1();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
---
+++ model/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model
+{
+   public static class Class1
+    {
+        public static void methodTest1()
+        {
+            Console.WriteLine("methodTest1 is running");
+        }
+
+        static void Main()
+        {
+
+        }
+    }
+}
