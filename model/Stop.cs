2b60a41e5a8c4dc2337a683e0f17284d6fd6d1ac
vardo159 <braianelsociales@gmail.com>
2019-09-14 17:35:36 -0500

    Stop class was initialized

--- model/Stop.cs
+++ model/Stop.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace model
 {
-    class Stop
+    public class Stop
     {
+        private String stopId;
+        private int planVersionId;
+        private String shortName;
+        private String longName;
+        private double gpsX;
+        private double gpsY;
+        private double longit;
+        private double latit;
+
+        public Stop(String stopId, int planVersionId, String shortName, String longName, double gpsX, double gpsY, double longit, double latit)
+        {
+            this.stopId = stopId;
+            this.planVersionId = planVersionId;
+            this.shortName = shortName;
+            this.longName = longName;
+            this.gpsX = gpsX;
+            this.gpsY = gpsY;
+            this.longit = longit;
+            this.latit = latit;
+        }
+
+        public String StopId { get => stopId; set => stopId = value; }
+        public double Longit { get => longit; set => Longit = value; }
+        public double Latit { get => latit; set => latit = value; }
+
+
     }
 }
