2e89fea454d9ea2b2356d6fab5fa3f31fbf079a9
Lize756 <lize756cas@gmail.com>
2019-09-15 15:32:26 -0500

    aggregate reading method

--- model/Principal.cs
+++ model/Principal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,16 +8,78 @@ using System.Threading.Tasks;
 namespace model
 {
 
-   public static class Principal
+    public class Principal
     {
-        public static void methodTest1()
+        private List<Stop> stops;
+
+        public Principal()
         {
-            Console.WriteLine("methodTest1 is running");
+            stops = new List<Stop>();
         }
 
-        static void Main()
-        {
+        public List<Stop> Stops { get => stops; set => stops = value; }
+
+        public void addStops() {
+            //StreamReader leer = new StreamReader(Path.GetFullPath("stops.txt"));
+            StreamReader leer = new StreamReader(Path.GetFullPath("StopPratice.txt"));
+            String line = leer.ReadLine();
+            Stop newS = null;
+
+            try
+            {
+                while (line != null) {
+                    String[] lineS = line.Split(',');
+                    if (lineS.Length == 8) {
+                        newS = new Stop(lineS[0], Int32.Parse(lineS[1]), lineS[2], lineS[3], Double.Parse(lineS[4]), Double.Parse(lineS[5]), Double.Parse(lineS[6]), Double.Parse(lineS[7]));
+                        stops.Add(newS);
+                        Console.WriteLine(newS.StopId);
+                    }
+                }
+                line = leer.ReadLine();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
 
         }
+
+        public Stop SearchStops(String IDStop) {
+
+            Stop search = null;
+            int left, right, means;
+            left = 0;
+            right = stops.Count - 1;
+            means = (right - left) / 2;
+
+            while (IDStop != stops[means].StopId && left < right) {
+                int comparations = IDStop.CompareTo(stops[means].StopId);
+
+                if (comparations > 0) {
+                    left = means + 1;
+                } else if (comparations < 0) {
+                    right = means - 1;
+                }
+                means = (right + right) / 2;
+            }
+            if (IDStop == stops[means].StopId)
+            {
+                search = stops[means];
+            }
+            else {
+                Console.WriteLine("Stop not found");
+            }
+
+            Console.WriteLine(search.StopId);
+            return search;
+        }
+
+       // public static void methodTest1()
+        //{ 
+          //  Console.WriteLine("methodTest1 is running");
+       // }
+
+        //static void Main()
+        //{
+        //}
     }
 }
