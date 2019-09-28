2bf1881833a9d57d3d1023f442af450d7499e6c6
vardo159 <braianelsociales@gmail.com>
2019-09-28 13:43:48 -0500

    update: label methods recovered

--- Proy_In/Form1.Designer.cs
+++ Proy_In/Form1.Designer.cs
@@ -36,19 +36,16 @@
             this.loadDataBt = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.locateBt = new System.Windows.Forms.Button();
-<<<<<<< HEAD
             this.testMotion = new System.Windows.Forms.Button();
-=======
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
->>>>>>> 84e37f7fb6a26a2d69acfe84845ce7f9675a5e81
             this.SuspendLayout();
             // 
             // map
@@ -60,7 +57,7 @@
             this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.map.LevelsKeepInMemmory = 5;
             this.map.Location = new System.Drawing.Point(-1, 1);
-            this.map.Margin = new System.Windows.Forms.Padding(2);
+            this.map.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.map.MarkersEnabled = true;
             this.map.MaxZoom = 100;
             this.map.MinZoom = 1;
@@ -74,17 +71,17 @@
             this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.map.ShowTileGridLines = false;
-            this.map.Size = new System.Drawing.Size(806, 763);
+            this.map.Size = new System.Drawing.Size(1075, 939);
             this.map.TabIndex = 0;
             this.map.Zoom = 1D;
             this.map.Load += new System.EventHandler(this.Map_Load);
             // 
             // zoomIn
             // 
-            this.zoomIn.Location = new System.Drawing.Point(974, 578);
-            this.zoomIn.Margin = new System.Windows.Forms.Padding(2);
+            this.zoomIn.Location = new System.Drawing.Point(1299, 711);
+            this.zoomIn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.zoomIn.Name = "zoomIn";
-            this.zoomIn.Size = new System.Drawing.Size(58, 32);
+            this.zoomIn.Size = new System.Drawing.Size(77, 39);
             this.zoomIn.TabIndex = 1;
             this.zoomIn.Text = "+";
             this.zoomIn.UseVisualStyleBackColor = true;
@@ -93,29 +90,27 @@
             // zoomInfo
             // 
             this.zoomInfo.AutoSize = true;
-            this.zoomInfo.Location = new System.Drawing.Point(602, 41);
-            this.zoomInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.zoomInfo.Location = new System.Drawing.Point(803, 50);
             this.zoomInfo.Name = "zoomInfo";
-            this.zoomInfo.Size = new System.Drawing.Size(0, 13);
+            this.zoomInfo.Size = new System.Drawing.Size(0, 17);
             this.zoomInfo.TabIndex = 2;
             // 
             // actualZoom
             // 
             this.actualZoom.AutoSize = true;
-            this.actualZoom.Location = new System.Drawing.Point(935, 588);
-            this.actualZoom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.actualZoom.Location = new System.Drawing.Point(1247, 724);
             this.actualZoom.Name = "actualZoom";
-            this.actualZoom.Size = new System.Drawing.Size(35, 13);
+            this.actualZoom.Size = new System.Drawing.Size(46, 17);
             this.actualZoom.TabIndex = 3;
             this.actualZoom.Text = "label1";
             this.actualZoom.Click += new System.EventHandler(this.ActualZoom_Click);
             // 
             // zoomOut
             // 
-            this.zoomOut.Location = new System.Drawing.Point(869, 578);
-            this.zoomOut.Margin = new System.Windows.Forms.Padding(2);
+            this.zoomOut.Location = new System.Drawing.Point(1159, 711);
+            this.zoomOut.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.zoomOut.Name = "zoomOut";
-            this.zoomOut.Size = new System.Drawing.Size(62, 32);
+            this.zoomOut.Size = new System.Drawing.Size(83, 39);
             this.zoomOut.TabIndex = 4;
             this.zoomOut.Text = "-";
             this.zoomOut.UseVisualStyleBackColor = true;
@@ -123,10 +118,10 @@
             // 
             // loadDataBt
             // 
-            this.loadDataBt.Location = new System.Drawing.Point(857, 623);
-            this.loadDataBt.Margin = new System.Windows.Forms.Padding(2);
+            this.loadDataBt.Location = new System.Drawing.Point(1143, 767);
+            this.loadDataBt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.loadDataBt.Name = "loadDataBt";
-            this.loadDataBt.Size = new System.Drawing.Size(188, 27);
+            this.loadDataBt.Size = new System.Drawing.Size(251, 33);
             this.loadDataBt.TabIndex = 5;
             this.loadDataBt.Text = "Cargar archivo";
             this.loadDataBt.UseVisualStyleBackColor = true;
@@ -134,10 +129,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(935, 522);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(1247, 642);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
             this.button1.Text = "test";
             this.button1.UseVisualStyleBackColor = true;
@@ -145,33 +140,34 @@
             // 
             // locateBt
             // 
-            this.locateBt.Location = new System.Drawing.Point(857, 666);
-            this.locateBt.Margin = new System.Windows.Forms.Padding(2);
+            this.locateBt.Location = new System.Drawing.Point(1143, 820);
+            this.locateBt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.locateBt.Name = "locateBt";
-            this.locateBt.Size = new System.Drawing.Size(188, 27);
+            this.locateBt.Size = new System.Drawing.Size(251, 33);
             this.locateBt.TabIndex = 7;
             this.locateBt.Text = "Ubicar";
             this.locateBt.UseVisualStyleBackColor = true;
             this.locateBt.Click += new System.EventHandler(this.LocateBt_Click);
             // 
-<<<<<<< HEAD
             // testMotion
             // 
-            this.testMotion.Location = new System.Drawing.Point(764, 325);
+            this.testMotion.Location = new System.Drawing.Point(1143, 879);
+            this.testMotion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.testMotion.Name = "testMotion";
-            this.testMotion.Size = new System.Drawing.Size(119, 23);
+            this.testMotion.Size = new System.Drawing.Size(159, 28);
             this.testMotion.TabIndex = 8;
             this.testMotion.Text = "test animation";
             this.testMotion.UseVisualStyleBackColor = true;
             this.testMotion.Click += new System.EventHandler(this.TestMotion_Click);
-=======
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(878, 77);
+            this.label1.Location = new System.Drawing.Point(1171, 95);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 21);
+            this.label1.Size = new System.Drawing.Size(122, 26);
             this.label1.TabIndex = 8;
             this.label1.Text = "Filtrar por...";
             this.label1.Click += new System.EventHandler(this.Label1_Click);
@@ -179,9 +175,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1037, 367);
+            this.label2.Location = new System.Drawing.Point(1383, 452);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.Size = new System.Drawing.Size(52, 17);
             this.label2.TabIndex = 9;
             this.label2.Text = "Zonas:";
             this.label2.Click += new System.EventHandler(this.Label2_Click);
@@ -189,9 +186,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(870, 121);
+            this.label3.Location = new System.Drawing.Point(1160, 149);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.Size = new System.Drawing.Size(133, 17);
             this.label3.TabIndex = 10;
             this.label3.Text = "Paradas en la calle:";
             this.label3.Click += new System.EventHandler(this.Label3_Click);
@@ -199,9 +197,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(892, 455);
+            this.label4.Location = new System.Drawing.Point(1189, 560);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.Size = new System.Drawing.Size(81, 17);
             this.label4.TabIndex = 11;
             this.label4.Text = "Estaciones:";
             this.label4.Click += new System.EventHandler(this.Label4_Click);
@@ -209,9 +208,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(892, 387);
+            this.label5.Location = new System.Drawing.Point(1189, 476);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.Size = new System.Drawing.Size(51, 17);
             this.label5.TabIndex = 12;
             this.label5.Text = "Buses:";
             this.label5.Click += new System.EventHandler(this.Label5_Click);
@@ -220,21 +220,11 @@
             // 
             this.panel1.Controls.Add(this.checkedListBox1);
             this.panel1.Controls.Add(this.label6);
-            this.panel1.Location = new System.Drawing.Point(928, 169);
+            this.panel1.Location = new System.Drawing.Point(1237, 208);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(189, 195);
+            this.panel1.Size = new System.Drawing.Size(252, 240);
             this.panel1.TabIndex = 13;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(14, 11);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(68, 24);
-            this.label6.TabIndex = 14;
-            this.label6.Text = "Zonas";
-            this.label6.Click += new System.EventHandler(this.Label6_Click);
             // 
             // checkedListBox1
             // 
@@ -249,29 +239,37 @@
             "Zona 6 (Guadalupe)",
             "Zona 7 (Cañaveralejo)",
             "Zona 8 (Calipso)"});
-            this.checkedListBox1.Location = new System.Drawing.Point(3, 38);
+            this.checkedListBox1.Location = new System.Drawing.Point(4, 47);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(182, 139);
+            this.checkedListBox1.Size = new System.Drawing.Size(241, 157);
             this.checkedListBox1.TabIndex = 15;
->>>>>>> 84e37f7fb6a26a2d69acfe84845ce7f9675a5e81
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(19, 14);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 29);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Zonas";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(922, 693);
-            this.Controls.Add(this.testMotion);
-=======
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1156, 766);
+            this.ClientSize = new System.Drawing.Size(1541, 943);
+            this.Controls.Add(this.testMotion);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
->>>>>>> 84e37f7fb6a26a2d69acfe84845ce7f9675a5e81
             this.Controls.Add(this.locateBt);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.loadDataBt);
@@ -280,7 +278,7 @@
             this.Controls.Add(this.zoomInfo);
             this.Controls.Add(this.zoomIn);
             this.Controls.Add(this.map);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -301,9 +299,9 @@
         private System.Windows.Forms.Button loadDataBt;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button locateBt;
-<<<<<<< HEAD
+
         private System.Windows.Forms.Button testMotion;
-=======
+
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -312,7 +310,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
->>>>>>> 84e37f7fb6a26a2d69acfe84845ce7f9675a5e81
+
     }
 }
 
